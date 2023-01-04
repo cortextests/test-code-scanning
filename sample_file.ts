d6ad7298324e50a737504b0dffcb234034d669b1
Gregory Pett <greg@Gregorys-MacBook-Pro.local>
2023-01-04 15:25:47 -0500

    adding to test repo

--- sample_file.ts
+++ sample_file.ts
@@ -1,4 +1,13 @@
-export interface Sample {
-    name: string;
-    isTrue: boolean
-}
+var express = require('express');
+var app = express();
+
+app.post("/foo", (req, res) => {
+    var obj = req.body;
+
+    var ret = [];
+
+    // Potential DoS if obj.length is large.
+    for (var i = 0; i < obj.length; i++) {
+        ret.push(obj[i]);
+    }
+});
